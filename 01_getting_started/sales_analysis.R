90cc081b3594069881a2d75e88b2ce7f6d12f6ca
yassermakki <yasser.makki@tuhh.de>
2020-12-01 17:45:27 +0100

    update

---
+++ 01_getting_started/sales_analysis.R
@@ -0,0 +1,46 @@
+
+
+## 1.1 Load libraries ----
+library(tidyverse)
+library(lubridate)
+library(readxl)
+library("writexl")
+
+## 1.2 Importing Files ----
+bikes_tbl      <- read_excel(path = "00_data/01_bike_sales/01_raw_data/bikes.xlsx")
+orderlines_tbl <- read_excel("00_data/01_bike_sales/01_raw_data/orderlines.xlsx")
+bikeshops_tbl  <- read_excel("00_data/01_bike_sales/01_raw_data/bikeshops.xlsx")
+
+## 1.3 Examining Data ----
+
+
+## 1.4 Joining Data ----
+
+
+## 1.5 Wrangling Data ----
+
+
+## 6.0 Business Insights ----
+## 6.1 Sales by Year ----
+
+## Step 1 - Manipulate
+
+## Step 2 - Visualize
+
+
+## 6.2 Sales by Year and Category 2 ----
+
+## Step 1 - Manipulate
+
+## Step 2 - Visualize
+
+
+
+## 7.0 Writing Files ----
+
+## 7.1 Excel ----
+
+## 7.2 CSV ----
+
+## 7.3 RDS ----
+
